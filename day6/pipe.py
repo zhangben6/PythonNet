94e47d2fccafe09425878f1c4e1405d9fca306a2
zhangben6 <1457287508@qq.com>
2018-11-17 14:45:48 +0800

    das

--- day6/pipe.py
+++ day6/pipe.py
@@ -1,24 +1,24 @@
-# from multiprocessing import Process,Pipe
-# import os,time
+from multiprocessing import Process,Pipe
+import os,time
 
-# fd1,fd2 = Pipe() #管道参数默认为True,表示双向管道
-# def fun(name):
-#     time.sleep(3)
-#     fd1.send("hello! "+ name.title())
+fd1,fd2 = Pipe() #管道参数默认为True,表示双向管道
+def fun(name):
+    time.sleep(3)
+    fd1.send("hello! "+ name.title())
 
-# jobs = []
-# for i in ['tom','zack','abby','sam']:
-#     p = Process(target=fun,args=(i,))
-#     jobs.append(p)
-#     p.start()
+jobs = []
+for i in ['tom','zack','abby','sam']:
+    p = Process(target=fun,args=(i,))
+    jobs.append(p)
+    p.start()
 
-# #从管道读取内容
-# for i in range(4):
-#     data = fd2.recv()
-#     print(data)
+#从管道读取内容
+for i in range(4):
+    data = fd2.recv()
+    print(data)
 
-# for i in jobs:
-#     i.join()
+for i in jobs:
+    i.join()
 
 
 

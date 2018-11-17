33d3e36dfe4592d2d38754f2c9bc962f73f3ba89
zhangben6 <1457287508@qq.com>
2018-11-17 14:36:59 +0800

    一点点修改

--- day6/pool.py
+++ day6/pool.py
@@ -13,7 +13,7 @@ result = []
 #向进程池添加事件
 for i in range(10):
     msg = "hello %d" % i
-    r = pool.apply_async(func=worker,args=(msg,))
+    r = pool.apply_async(func=worker,args=(msg,)) #返回值是一个对象
     result.append(r)
 
     #将事件同步方式放入进程池
@@ -28,5 +28,6 @@ print("===================================")
 print(result)
 for i in result:
     print(i.get())
+    #函数对象本身有一个方法get()方法,可以获取进程函数的返回值
 
 

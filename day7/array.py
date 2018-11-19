e3e9beb4e9a151539b8aa531229bacb8041b85c1
zhangben6 <1457287508@qq.com>
2018-11-19 10:52:32 +0800

    向共享内存中传入数组或者创建

---
+++ day7/array.py
@@ -0,0 +1,33 @@
+'''the biggest difference with 
+Value is incoming parameters must be
+integer or array
+'''
+# from multiprocessing import Process,Array
+# import time
+# shm = Array('i',[1,2,3,4,5,6])
+# def run():
+#     for i in shm:
+#         print(i)
+#     shm[0]=1000
+# p = Process(target=run)
+# p.start()
+# p.join()
+# for i in shm:
+#     print(i,end=' ')
+# print()
+
+
+from multiprocessing import Process,Array
+import time
+shm = Array('c',b'hello')
+def run():
+    for i in shm:
+        print(i)
+    shm[0]=b'H'
+p = Process(target=run)
+p.start()
+p.join()
+for i in shm:
+    print(i,end=' ')
+print()
+print(shm.value)

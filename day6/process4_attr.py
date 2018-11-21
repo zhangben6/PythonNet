3ff23fbe0c1d6d4f65a0b3b811c39c8bf1261c29
zhangben6 <1457287508@qq.com>
2018-11-21 15:38:48 +0800

    笔记更新

--- day6/process4_attr.py
+++ day6/process4_attr.py
@@ -15,6 +15,23 @@
 # p.join()
 
 
+# from multiprocessing import Process
+# from time import sleep,ctime
+
+# def tm():
+#     for i in range(4):
+#         sleep(2)
+#         print(ctime())
+
+# p = Process(target=tm,name='张奔')
+# p.daemon = True
+# p.start()
+# #打印进程对象属性
+# print('name:',p.name)
+# print('pid:',p.pid)
+# print("alive:",p.is_alive())
+# # p.join()
+
 from multiprocessing import Process
 from time import sleep,ctime
 
@@ -24,10 +41,11 @@ def tm():
         print(ctime())
 
 p = Process(target=tm,name='张奔')
-p.daemon = True
+# p.daemon = True
 p.start()
 #打印进程对象属性
-print('name:',p.name)
-print('pid:',p.pid)
-print("alive:",p.is_alive())
+
+while True:
+    pass
+    input(">>>")
 # p.join()

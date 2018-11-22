dbc2cc566f18a67164b3713722192688a8d29096
zhangben6 <1457287508@qq.com>
2018-11-22 21:06:25 +0800

    传递文件

--- day2/server.py
+++ day2/server.py
@@ -1,18 +1,40 @@
-from socket import *
+# from socket import *
 
+# s = socket()
+# s.bind(("0.0.0.0",9800))
+# s.listen(5)
+# print("Waiting for ....")
+# c,addr = s.accept()
+# print("Connect for:",addr)
+# f  = open("recv4.jpg","wb")
+# while True:
+#     data = c.recv(1024)
+#     if not data:
+#         break
+#     f.write(data)
+#     # c.send("我成功接受并打印THANKS".encode())
+# c.close()
+# f.close()
+# s.close()
+
+#需求:接受客户端发来的文件:
+from socket import *
 s = socket()
-s.bind(("0.0.0.0",9800))
+s.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+s.bind(("0.0.0.0",8000))
 s.listen(5)
-print("Waiting for ....")
+print("waiting...")
 c,addr = s.accept()
-print("Connect for:",addr)
-f  = open("recv4.jpg","wb")
+try:
+    f = open('recv.jpg','wb')
+except:
+    print("打开文件失败")
 while True:
     data = c.recv(1024)
     if not data:
         break
     f.write(data)
-    # c.send("我成功接受并打印THANKS".encode())
-c.close()
+    # c.send(b'OK')
 f.close()
+c.close()
 s.close()
